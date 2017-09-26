84048d9aab375a5c969f1257a81a8f942b0f3ed2
Mark Blanco <markb1@hestia.andrew.local.cmu.edu>
2017-09-26 16:41:14 -0400

    Add lab2 part 2 sublab 3 files. That's not confusing...

---
+++ RecComp/Lab2/ug871-design-files/Introduction/lab2/fir_prj/vivado_hls.app
@@ -0,0 +1,16 @@
+<AutoPilot:project xmlns:AutoPilot="com.autoesl.autopilot.project" projectType="C/C++" name="fir_prj" top="fir">
+    <includePaths/>
+    <libraryFlag/>
+    <files>
+        <file name="../../fir_test.c" sc="0" tb="1" cflags=" "/>
+        <file name="../../out.gold.dat" sc="0" tb="1" cflags=" "/>
+        <file name="fir.c" sc="0" tb="false" cflags=""/>
+    </files>
+    <solutions>
+        <solution name="solution1" status=""/>
+    </solutions>
+    <Simulation argv="">
+        <SimFlow name="csim" setup="false" optimizeCompile="false" clean="false" ldflags="" mflags=""/>
+    </Simulation>
+</AutoPilot:project>
+
---
+++ RecComp/Lab2/ug871-design-files/Introduction/lab2/run_hls.tcl
@@ -0,0 +1,19 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project -reset fir_prj
+set_top fir
+add_files fir.c
+add_files -tb out.gold.dat
+add_files -tb fir_test.c
+open_solution -reset "solution1"
+set_part {xc7k160tfbg484-2} -tool vivado
+create_clock -period 10 -name default
+#source "./fir_prj/solution1/directives.tcl"
+csim_design -compiler gcc
+csynth_design
+cosim_design
+export_design -rtl verilog -format ip_catalog
+exit
